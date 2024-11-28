a3daa5631c6fcd83fab872fe962f7792f1b3e27d
KhanhLy1401 <khanhlypt14@gmail.com>
2024-11-28 18:23:07 +0700

    Layout Resizable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage.tsx"
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage.tsx";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout.tsx";
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage />}/>
         <Route path='/sso-callback' element={<AuthenticateWithRedirectCallback 
           signUpFallbackRedirectUrl={"/auth-callback"}
         />}/>
         <Route path='/auth-callback' element={<AuthCallbackPage />} />
+        <Route element={<MainLayout />}>
+          <Route path='/' element={<HomePage />}/>
+        
+        </Route>
 
       </Routes>
     </>
